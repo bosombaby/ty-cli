3ccebaa0b9f3f34e4478683075b7a20654818062
bosom <1245924849@qq.com>
2024-06-28 11:49:31 +0800

    feat: 文件模块

--- bin/enter.js
+++ bin/enter.js
@@ -1,1 +1,32 @@
 #! /usr/bin/env node
+import chalk from "chalk";
+import { Command } from "commander";
+import { handleOptions } from "../lib/create.js";
+
+const program = new Command();
+// 版本信息
+program
+  .name("ty-cli")
+  .usage("<command> [options]")
+  .description("A simple CLI tool for string manipulation")
+  .version("0.0.1");
+
+// 帮助信息
+program.on("--help", () => {
+  console.log(
+    `\nRun ${chalk.cyan(
+      "ty-cli <command> --help"
+    )} for detailed usage of given command.\n`
+  );
+});
+
+// 创建项目命令
+program
+  .command("create <project-name>")
+  .description("create a new project")
+  .option("-f, --force", "overwrite target directory if it exists")
+  .action((projectName, options) => {
+    handleOptions(projectName, options);
+  });
+
+program.parse();
