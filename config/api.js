9423beb425374afee277e0ff263e2e8c8b4e9d1a
bosombaby <1245924849@qq.com>
2024-07-08 13:43:53 +0800

    feat: 权限

--- config/api.js
+++
@@ -1,7 +0,0 @@
-const BASE_REPOSITORY_URL =
-  "https://api.github.com/repos/bosombaby/ty-cli-template";
-
-const FINE_GRAINED_TOKEN =
-  "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";
-
-export { BASE_REPOSITORY_URL, FINE_GRAINED_TOKEN };
