90c97a25fddba0a9186dfe56c11595e164360175
bosombaby <1245924849@qq.com>
2024-07-09 18:58:03 +0800

    feat: 帮助手册完善

--- bin/enter.js
+++ bin/enter.js
@@ -1,23 +1,31 @@
 #! /usr/bin/env node
 import chalk from "chalk";
 import { Command } from "commander";
-import { handleOptions } from "../lib/create.js";
+import { handleOptions } from "../cmd/create.js";
+import { openDefaultBrowser } from "../lib/browser.js";
+import { getPackageJson } from "../lib/file.js";
 
+const config = getPackageJson();
 const program = new Command();
 // 版本信息
 program
-  .name("ty-cli")
+  .name(config.name)
   .usage("<command> [options]")
   .description("A simple CLI tool for string manipulation")
-  .version("1.1.0");
+  .version(config.version);
 
 // 帮助信息
 program.on("--help", () => {
   console.log(
-    `\nRun ${chalk.cyan(
+    `\r\nRun ${chalk.cyan(
       "ty-cli <command> --help"
-    )} for detailed usage of given command.\n`
+    )} for detailed usage of given command.\r\n`
   );
+  console.log(
+    `Ty CLI Docs Address At ${chalk.red("https://ty.cli.vrteam.top/")}\r\n`
+  );
+
+  openDefaultBrowser("https://ty.cli.vrteam.top/");
 });
 
 // 创建项目命令
